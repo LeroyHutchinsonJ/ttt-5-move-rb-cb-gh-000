609f5a7441f3b2333a6095c7101581a3b0b7e5f3
Leroy Hutchinson <l.hutchinsonj@gmail.com>
2018-06-15 22:02:41 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -1,17 +1,18 @@
-
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
 
 # code your input_to_index and move method here!
 
-def update_array_at_with(board,index,value)
-board[index] = value
+def input_to_index(index)
+trueindex = index.to_i + 1
+return trueindex
+end
 
-
-
-puts " #{board[0]} | #{board[1]} | #{board[2]} "
-puts "-----------"
-puts " #{board[3]} | #{board[4]} | #{board[5]} "
-puts "-----------"
-puts " #{board[6]} | #{board[7]} | #{board[8]} "
-
-
+def update_array_at_with(array,index,value)
+array[index] = value
 end
