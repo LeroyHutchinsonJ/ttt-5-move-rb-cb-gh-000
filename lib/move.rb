0ebac976776eacc82320dc0afbbe961063933b05
Leroy Hutchinson <l.hutchinsonj@gmail.com>
2018-06-15 21:39:44 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -9,8 +9,9 @@ end
 # code your input_to_index and move method here!
 
 def input_to_index(index)
-
 trueIndex = index.to_i + 1
+end
 
-
+def update_array_at_with(array,index,value)
+array[index] = value
 end
