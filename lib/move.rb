cce23f59dc3f7e7f3b9e5ffbec1bbd6412ea3744
Leroy Hutchinson <l.hutchinsonj@gmail.com>
2018-06-15 22:14:01 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -10,6 +10,13 @@ end
 
 
 #Takes user input and uses it to fill out position that user inputed
-def input_to_index(index.to_i + 1)
+def input_to_index(index.to_i + 1, board)
 board[index] =
+
+puts " #{board[0]} | #{board[1]} | #{board[2]} "
+puts "-----------"
+puts " #{board[3]} | #{board[4]} | #{board[5]} "
+puts "-----------"
+puts " #{board[6]} | #{board[7]} | #{board[8]} "
+
 end
