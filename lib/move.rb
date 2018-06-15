37088ef6152b59821a739f17c837e85efde2b7f3
Leroy Hutchinson <l.hutchinsonj@gmail.com>
2018-06-15 22:07:52 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -9,7 +9,7 @@ end
 def input_to_index(index,value)
 
   board[index] = "X"
-  
+
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
