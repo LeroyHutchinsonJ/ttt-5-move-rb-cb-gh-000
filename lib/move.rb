365df95d11be86653ce97b4b5ecc4f52a37b60ac
Leroy Hutchinson <l.hutchinsonj@gmail.com>
2018-06-15 22:10:00 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -1,19 +1,15 @@
 def display_board(board)
-
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
 # code your input_to_index and move method here!
 
 
 #Takes user input and uses it to fill out position that user inputed
-def input_to_index(index,value)
-
-  board[index] = "X"
-
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
-
+def input_to_index(index)
+board[index] = 
 end
