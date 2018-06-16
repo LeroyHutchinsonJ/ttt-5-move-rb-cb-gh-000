ea41f0bf36280f73424b09cc0df71a6c3fddc87b
Leroy Hutchinson <l.hutchinsonj@gmail.com>
2018-06-16 00:01:22 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -10,10 +10,7 @@ end
 
 
 #Takes user input and uses it to fill out position that user inputed
-def input_to_index(index)
-trueIndex = index.to_i - 1
-return trueIndex
-end
+def input_to_index(input) index = input.to_i return index -= 1 end
 
 def move(array, index, value = "X")
   trueIndex = index.to_i
